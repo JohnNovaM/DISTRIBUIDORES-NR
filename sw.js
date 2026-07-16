96fac2b6e87400fecbe12358bef250566b2a4bef
JohnNovaM <128113211+JohnNovaM@users.noreply.github.com>
2026-07-15 23:21:14 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  *    la versión vieja de la app aunque tú ya hayas subido cambios.
  ***********************************************************************/
 
-var CACHE_VERSION = 'v4';
+var CACHE_VERSION = 'v5';
 var CACHE_NAME = 'natural-resvex-' + CACHE_VERSION;
 
 // Archivos del "cascarón" de la app (shell): lo mínimo para que la
